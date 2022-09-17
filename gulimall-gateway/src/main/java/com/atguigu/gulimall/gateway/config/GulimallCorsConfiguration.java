d7661ec9b3d926d741db04ce6eebbc7da67be850
null <null@github.com>
2022-09-17 15:05:43 +0800

    多环境适配

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GulimallCorsConfiguration.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GulimallCorsConfiguration.java
@@ -6,14 +6,16 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 
 @Configuration // gateway
 public class GulimallCorsConfiguration {
 
     @Bean // 添加过滤器
-    public CorsWebFilter corsWebFilter(){
+    public CorsWebFilter corsWebFilter() {
         // 基于url跨域，选择reactive包下的
-        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         // 跨域配置信息
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         // 允许跨域的头
@@ -21,12 +23,19 @@ public class GulimallCorsConfiguration {
         // 允许跨域的请求方式
         corsConfiguration.addAllowedMethod("*");
         // 允许跨域的请求来源
-        corsConfiguration.addAllowedOriginPattern("*");
+//        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://cloud", "http://localhost:8080",
+                "http://localhost:8090",
+                "http://ruoyi.com:88",
+                "http://cloud:88",
+                "http://cloud:80"
+        ));
         // 是否允许携带cookie跨域
+
         corsConfiguration.setAllowCredentials(true);
 
         // 任意url都要进行跨域配置
-        source.registerCorsConfiguration("/**",corsConfiguration);
+        source.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsWebFilter(source);
     }
 }
